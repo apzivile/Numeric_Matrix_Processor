97210cb6fc50896b00923c31e1f0757a62a73ca1
apzivile <zivile.apo@gmail.com>
2020-07-10 16:36:28 +0100

    Numeric Matrix Processor project; recursion problems

--- Problems/Number of decompositions/src/Main.java
+++ Problems/Number of decompositions/src/Main.java
@@ -1,5 +1,27 @@
-class Main {
-    public static void main(String[] args) {
-        // put your code here
+import java.util.Scanner;
+
+public class Main {
+
+    public static void partition(int n) {
+        partition(n, n, "");
     }
+    public static void partition(int n, int max, String prefix) {
+
+        if (n == 0) {
+            System.out.println(prefix);
+            return;
+        }
+        for (int i =1 ; i <= Math.min(max, n); i++) {
+            partition(n-i, i, prefix + " " + i);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        partition(n);
+    }
+
 }
